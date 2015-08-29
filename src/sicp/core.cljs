0d5b18e83849b707d524f4819150c88a220a19a8
David Wilhelm <dewilhelm@gmail.com>
2015-08-28 22:23:00 -0400

    add wave painter

--- src/sicp/core.cljs
+++ src/sicp/core.cljs
@@ -73,13 +73,13 @@
 (defn end-segment [segment]
   (nth segment 1))
 
-
 ;;draw a line on the canvas
+;;offset by .5 to keep lines sharp
 (defn draw-line! [start end]
     (println start end)
   (.beginPath CTX)
-  (.moveTo CTX (xcor-vect start) (ycor-vect start))
-  (.lineTo CTX (xcor-vect end) (ycor-vect end))
+  (.moveTo CTX (+ 0.5 (xcor-vect start)) (+ 0.5 (ycor-vect start)))
+  (.lineTo CTX (+ 0.5(xcor-vect end)) (+ 0.5 (ycor-vect end)))
   (.stroke CTX))
 
 ;;Painter
@@ -97,7 +97,7 @@
 (defn clear-canvas []
   (.clearRect CTX 0 0 CANVAS_WIDTH CANVAS_HEIGHT))
 
-(def FRAME (make-frame [0 0] [0 500] [500 0]))
+(def FRAME (make-frame [0 0] [500 0] [0 500]))
 ;;test
 ;;(draw-line! (make-vect 0 0) (make-vect 500 500))
 
@@ -109,10 +109,48 @@
                         (make-segment (make-vect 1 0) (make-vect 0 0))]
                        ) frame))
 
-;;a) (outline-frame FRAME)
+(defn X-frame [frame]
+  ((segments->painter [(make-segment [0 0] [1 1])
+                       (make-segment [0 1] [1 0])])
+   frame))
 
+(defn diamond-frame [frame]
+  ((segments->painter [(make-segment [0.5 0] [1 0.5])
+                       (make-segment [1 0.5] [0.5 1])
+                       (make-segment [0.5 1] [0 0.5])
+                       (make-segment [0 0.5] [0.5 0])
+                     ]) frame))
 
+;;define WAVE segments, normalizing co-ords
+(defn make-normalize-fn [width height]
+  (fn [vect]
+    (make-vect (/ (xcor-vect vect) width) (/ (ycor-vect vect) height))))
 
-
- 
-
+(defn wave [frame]
+  (let [N (make-normalize-fn 14 20)]
+    ((segments->painter [(make-segment (N [6 10]) (N [0 10]))
+                         (make-segment (N [0 10]) (N [0 8]))
+                         (make-segment (N [0 8]) (N [6 8]))
+                         (make-segment (N [6 8]) (N [6 6]))
+                         (make-segment (N [6 6]) (N [4 4]))
+                         (make-segment (N [4 4]) (N [4 2]))
+                         (make-segment (N [4 2]) (N [6 0]))
+                         (make-segment (N [6 0]) (N [8 0]))
+                         (make-segment (N [8 0]) (N [10 2]))
+                         (make-segment (N [10 2]) (N [10 4]))
+                         (make-segment (N [10 4]) (N [8 6]))
+                         (make-segment (N [8 6]) (N [8 8]))
+                         (make-segment (N [8 8]) (N [10 8]))
+                         (make-segment (N [10 8]) (N [10 6]))
+                         (make-segment (N [10 6]) (N [12 6]))
+                         (make-segment (N [12 6]) (N [12 10]))
+                         (make-segment (N [12 10]) (N [8 10]))
+                         (make-segment (N [8 10]) (N [8 12]))
+                         (make-segment (N [8 12]) (N [10 20]))
+                         (make-segment (N [10 20]) (N [8 20]))
+                         (make-segment (N [8 20]) (N [7 16]))
+                         (make-segment (N [7 16]) (N [6 20]))
+                         (make-segment (N [6 20]) (N [4 20]))
+                         (make-segment (N [4 20]) (N [6 12]))
+                         (make-segment (N [6 12]) (N [6 10]))
+                         ]) frame)))
